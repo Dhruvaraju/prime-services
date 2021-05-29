b1d2ea81f4582253dc46a3b64a6034dbbdacb115
Dhruva Raju <49dhruva@gmail.com>
2021-05-29 11:39:09 +0530

    feat(enabled): cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
 const app = express();
 dotenv.config();
 const PORT = process.env.port;
 app.set('port', (process.env.PORT || 5000));
+app.use(cors())
 
 app.use(express.json());
 const registerRoutes = require("./routes/registerRoutes");
